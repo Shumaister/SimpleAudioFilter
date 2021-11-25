9ecbf3c4839bdc2b87a8b671bb12533082fb724e
shumaister <gabrielmaiori@gmail.com>
2021-11-25 01:28:56 -0300

    rename principal script and fixes. Adding short song

--- SimpleAudioFilter.py
+++ SimpleAudioFilter.py
@@ -14,10 +14,7 @@ wav_fname = song
 
 # Filter requirements.
 order = 6
-#fs = 44100.0        # sample rate, Hz
 cutoff = 500.0       # desired cutoff frequency of the filter, Hz
-
-cutoff_high = 5000.0 
 
 def butter_lowpass(cutoff, fs, order=5):
     nyq = 0.5 * fs
@@ -34,31 +31,32 @@ def butter_lowpass_filter(data, cutoff, fs, order=5):
 #read the wav file
 samplerate, data = wavfile.read(wav_fname)
 fs = samplerate
+print(f"frequency Hz = {samplerate}")
 print(f"number of channels = {data.shape[1]}")
-samples = data.shape[0]                         # nro de muestras que hay
+samples = data.shape[0]  # 
+print(f"number of samples = {data.shape[0]}")
 length = samples / samplerate
 print(f"length = {length}s")
 time = np.linspace(0., length, data.shape[0])
 
-#Drawing the audio file --- el del entrada
+#Drawing the input audio file 
 plt.subplot(3, 2, 1)
 plt.plot(time, data[:, 0], label="Left channel", color='blue')
-#plt.plot(time, data[:, 1], label="Right channel", color='orange')
+# plt.plot(time, data[:, 1], label="Right channel", color='orange')
 plt.legend()
 plt.ylim(-30000, +30000)
 plt.xlabel("Time [s]")
 plt.ylabel("Amplitude")
 plt.grid()
 
-#input audio FFT (Spectrum) -- Tranfformada de fourier
+#input audio FFT (Spectrum)
 left_ch_fft = scipy.fftpack.fft(data[:, 0])
 left_ch_fft_frec = scipy.fftpack.fftfreq(len(left_ch_fft), 1.0 / fs)
 
 plt.subplot(3, 2, 2)
-plt.xscale('log')
-#x_scale = np.linspace(0.0 , samplerate / samples, int(samples /2))
-signal_psd = np.abs(left_ch_fft[:samples//2]) ** 2   # se divide por dos porqiue es simetrica respeto a Y
-plt.plot(left_ch_fft_frec[:samples//2], 10 * np.log10(signal_psd), label="Left channel FFT", color='red') #al hacerle log 10 lo pasas a db
+plt.xscale('log') 
+signal_psd = np.abs(left_ch_fft[:samples//2]) ** 2   
+plt.plot(left_ch_fft_frec[:samples//2], 10 * np.log10(signal_psd), label="Left channel FFT", color='red') #log 10 -> db
 plt.legend()
 plt.xlim(10, 0.5*fs)
 plt.xlabel("Frecuency [Hz]")
@@ -68,8 +66,8 @@ plt.grid(which='both')
 
 # Get the filter coefficients so we can check its frequency response.
 b, a = butter_lowpass(cutoff, fs, order)
-print(a) # polinomio del numerador - normalizado - CEROS
-print(b) # polinomio del denominador - POLOS
+print(a) # numerator polynomial - CEROS
+print(b) # denominator polynomialr - POLOS
 
 # Plot the frequency response.
 w, h = freqz(b, a, worN=8000)
@@ -116,7 +114,7 @@ plt.grid(which='both')
 outdata = data
 outdata[:,0] = outch1
 outdata[:,1] = outch2
-wav_fname = "out_filtered.wav"
+wav_fname = "out_filtered_"+sys.argv[2]+".wav"
 wavfile.write(wav_fname, int(fs), outdata.astype(np.int16))
 
 #Draws the plot
